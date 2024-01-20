68b2ad3ee7fcdd2bd970bc57297225a364c45c19
Meyyappan R <meyyappan721@gmail.com>
2024-01-20 18:30:58 +0530

    sign-up ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,7 +12,7 @@ export default function Header() {
         </h1>
         </Link>
         <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-            <input type="text" placeholder="Search..." className='bg-transparent focus:outline-none w-24 sm:64'/>
+            <input type="text" placeholder="Search..." className='bg-transparent focus:outline-none w-24 sm:w-64'/>
             <FaSearch className='text-slate-600'/>
         </form>
         <ul className='flex gap-4'>
@@ -23,7 +23,7 @@ export default function Header() {
             <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
             </Link>
             <Link to='/sign-in'>
-            <li className='hidden sm:inline text-slate-700 hover:underline'>Sign In</li>
+            <li className=' text-slate-700 hover:underline'>Sign In</li>
             </Link>
         </ul>
         </div>
