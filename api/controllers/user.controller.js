b731105879f0376200474c9865a99a7324f9884a
Meyyappan R <meyyappan721@gmail.com>
2024-01-23 23:15:04 +0530

    delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -33,3 +33,16 @@ try {
     next(error)
 }
 }
+
+export const deleteUser = async (req, res, next) => {
+    if(req.user.id !== req.params.id) return next(errorHandler(401, "You can delete your account only!"));
+
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.clearCookie('access_token'); //clear cookie and response
+        res.status(200).json('User has been deleted');
+
+    } catch (error) {
+        next(error);
+    }
+}
