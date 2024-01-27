f7010cf17afa3134b67be08cf5c01d29f683497b
Meyyappan R <meyyappan721@gmail.com>
2024-01-27 21:39:13 +0530

    user listing deletion functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -129,6 +129,23 @@ setShowListingsError(true);
   }
 }
 
+const handleListingDelete = async (listingId) => {
+  try{
+   const res = await fetch(`/api/listing/delete/${listingId}`,{
+    method: 'DELETE',
+   });
+   const data = await res.json();
+   if(data.success===false){
+    console.log(data.message);
+    return;
+   }
+   setUserListings((prev) => prev.filter((listing) => listing._id !== listingId));
+  }
+  catch(error){
+    console.log(error.message);
+  }
+}
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl font-semibold text-center my-7'>
@@ -176,7 +193,7 @@ setShowListingsError(true);
             <p>{listing.name}</p>
           </Link>
           <div className='flex flex-col items-center'>
-          <button className='text-red-700 uppercase'>delete</button>
+          <button onClick={() => handleListingDelete(listing._id)} className='text-red-700 uppercase'>delete</button>
           <button className='text-green-700 uppercase'>edit</button>
             </div>
           </div>
