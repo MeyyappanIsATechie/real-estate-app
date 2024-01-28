7f2e6fb3a6f2a5b90d7be9232f31342f75f36206
Meyyappan R <meyyappan721@gmail.com>
2024-01-28 16:12:54 +0530

    image slider functionality using Swiper

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
+import Listing from "./pages/Listing";
 
 export default function App() {
   return (<Router>
@@ -21,6 +22,7 @@ export default function App() {
       <Route path="/create-listing" element={<CreateListing/>}/>
       <Route path="/update-listing/:listingId" element={<UpdateListing/>}/>
       </Route>
+      <Route path="/listing/:listingId" element={<Listing/>}/>
       <Route path="/about" element={<About/>}/>
     </Routes>
   </Router>);
