ab10a4cbe2c56e008420f25dbdc633d95ffe3580
Meyyappan R <meyyappan721@gmail.com>
2024-01-30 23:08:04 +0530

    home page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -25,7 +25,7 @@ export default function Header() {
 
   return (
     <header className='bg-slate-200 shadow-md'>
-        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-6'>
             <Link to='/'>
             <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
             <span className='text-slate-500'>Meyyappan</span>
